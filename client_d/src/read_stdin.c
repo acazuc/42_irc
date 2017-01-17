9ce24065d89c2bb85a6fbcf349b6bf6118c62c26
Jukino <acazuc@student.42.fr>
2017-01-17 18:39:06 +0100

    Ok

--- client_d/src/read_stdin.c
+++ client_d/src/read_stdin.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 18:01:05 by acazuc            #+#    #+#             */
-/*   Updated: 2017/01/17 17:37:10 by acazuc           ###   ########.fr       */
+/*   Updated: 2017/01/17 18:37:35 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@ void	read_stdin(t_env *env)
 
 	if (!FD_ISSET(0, &env->sets[0]))
 		return ;
-	ft_memcpy(env->buf_stdin.data, env->buf_stdin.data
-			+ env->buf_stdin.pos, env->buf_stdin.lim - env->buf_stdin.pos);
-	env->buf_stdin.pos = env->buf_stdin.lim - env->buf_stdin.pos;
+	env->buf_stdin.pos = env->buf_stdin.lim;
 	env->buf_stdin.lim = BUF_SIZE;
 	if (env->buf_stdin.pos == env->buf_stdin.lim)
-		ERROR("stdin buffer full, aboting");
+		ERROR("stdin buffer full, aborting");
 	if ((readed = read(0, env->buf_stdin.data + env->buf_stdin.pos
 					, env->buf_stdin.lim - env->buf_stdin.pos)) == -1)
 		ERROR("read() failed");
